6fa6abd1e8a7a2a6e421418c6cbe3a9deed0c38b
ldex <laurent.duveau+git@gmail.com>
2020-04-19 18:14:52 -0400

    scully routes config

--- scully.angular-demo-scully.config.js
+++ scully.angular-demo-scully.config.js
@@ -3,5 +3,12 @@ exports.config = {
   projectName: "angular-demo-scully",
   outDir: './dist/static',
   routes: {
+    '/products/:id': {
+        type: 'json',
+        productId: {
+            url: 'http://storerestservice.azurewebsites.net/api/products/',
+            property: 'id'
+        }
+    }
   }
 };
